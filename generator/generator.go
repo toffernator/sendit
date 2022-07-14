3f928c9efabeb063b542c32a2cbeef1432c38592
Christoffer Gram <chkg@itu.dk>
2022-07-14 16:19:58 +0200

    Paramet name, in, type

--- generator/generator.go
+++ generator/generator.go
@@ -8,12 +8,29 @@ import (
 )
 
 const (
-	PATHS = "paths"
+	PATHS            = "paths"
+	PARAMETERS       = "parameters"
+	PARAMETER_NAME   = "name"
+	PARAMETER_IN     = "in"
+	PARAMETER_TYPE   = "type"
+	PARAMETER_FORMAT = "format"
 )
 
 type PathItem struct {
 	Path       string
-	Operations []string
+	Operations []Operation
+}
+
+type Operation struct {
+	Verb       string
+	Parameters []Parameter
+}
+
+type Parameter struct {
+	Name   string
+	In     string
+	Type   string
+	Format string
 }
 
 var api map[string]interface{}
@@ -33,24 +50,71 @@ func ParseJSON(path string) {
 	json.Unmarshal([]byte(byteValue), &api)
 }
 
-func Paths() (pathItems []PathItem) {
-	paths := api[PATHS].(map[string]interface{})
-	pathItems = make([]PathItem, 0)
+func Paths() []PathItem {
+	pathItems := make([]PathItem, 0)
 
-	for p, ops := range paths {
-		operations := ops.(map[string]interface{})
-
+	paths := paths()
+	for p := range paths {
+		operations := Operations(p)
 		pathItem := &PathItem{
 			Path:       p,
-			Operations: make([]string, 0),
-		}
-
-		for op := range operations {
-			pathItem.Operations = append(pathItem.Operations, op)
+			Operations: operations,
 		}
 
 		pathItems = append(pathItems, *pathItem)
 	}
 
 	return pathItems
+}
+
+func Operations(path string) []Operation {
+	ops := make([]Operation, 0)
+
+	for v, oitem := range operations(path) {
+		params := oitem.(map[string]interface{})[PARAMETERS]
+		parameters := make([]Parameter, 0)
+		if params != nil {
+			parameters = parseParameter(params.([]interface{}))
+		}
+
+		operation := Operation{
+			Verb:       v,
+			Parameters: parameters,
+		}
+
+		ops = append(ops, operation)
+	}
+
+	return ops
+}
+
+func parseParameter(parameters []interface{}) []Parameter {
+	if parameters == nil {
+		return make([]Parameter, 0)
+	}
+
+	parsed := make([]Parameter, 0)
+	for _, p := range parameters {
+		raw := p.(map[string]interface{})
+		parameter := Parameter{
+			Name: raw[PARAMETER_NAME].(string),
+			In:   raw[PARAMETER_IN].(string),
+			Type: raw[PARAMETER_TYPE].(string),
+			// TODO coercing potentially nil values
+		}
+
+		parsed = append(parsed, parameter)
+	}
+
+	return parsed
+}
+
+func paths() map[string]interface{} {
+	return api[PATHS].(map[string]interface{})
+}
+
+func operations(path string) map[string]interface{} {
+	paths := paths()
+	operations := paths[path].(map[string]interface{})
+	return operations
 }
